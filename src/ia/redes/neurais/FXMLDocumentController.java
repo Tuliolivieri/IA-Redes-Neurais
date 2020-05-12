45d87301577e5a797f519979ff9e90ac3f9cdac2
Tuliolivieri <tuliolivieri@gmail.com>
2020-05-12 11:10:23 -0300

    troca de AnchorPane para BorderPane e criados os parâmentros de controle da rede

--- src/ia/redes/neurais/FXMLDocumentController.java
+++ src/ia/redes/neurais/FXMLDocumentController.java
@@ -5,12 +5,16 @@
  */
 package ia.redes.neurais;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.paint.Paint;
 
 /** 
  *
@@ -18,10 +22,24 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable
 {
+
+    @FXML
+    private ToggleGroup transferFunction;
+    @FXML
+    private JFXButton btAbrirArquivo;
+    @FXML
+    private JFXTextField tfArquivo;
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
         // TODO
     }    
+
+    @FXML
+    private void clkAbrirArquivo(ActionEvent event)
+    {
+        btAbrirArquivo.setStyle("-fx-border-color: #00ff33");
+        tfArquivo.setUnFocusColor(Paint.valueOf("#00ff33"));
+    }
     
 }
