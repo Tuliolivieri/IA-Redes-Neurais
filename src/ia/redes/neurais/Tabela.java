84925b84528815e01c4f16aa364566984abbe053
Tuliolivieri <tuliolivieri@gmail.com>
2020-06-22 20:20:38 -0300

    correção na classe

--- src/ia/redes/neurais/Tabela.java
+++ src/ia/redes/neurais/Tabela.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -35,41 +35,3 @@ public class Tabela
     
     
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ia.redes.neurais;
-
-/**
- *
- * @author tulio
- */
-public class Tabela
-{
-    private String[] dados;
-
-    public String[] getDados()
-    {
-        return dados;
-    }
-
-    public void setDados(String[] dados)
-    {
-        this.dados = dados;
-    }
-
-    public Tabela()
-    {
-    }
-
-    public Tabela(String[] dados)
-    {
-        this.dados = dados;
-    }
-    
-    
-}
->>>>>>> ca6497eed2c850be40c97c42669abe8f30851571
